98925cd5bdd07bc11d4ef84f53378413c553774b
pbullion <pbullion@gmail.com>
2020-02-27 17:08:01 -0600

    rsvp list

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const pool = require("../db");
 const fetch = require("node-fetch");
 const router = Router();
+var moment = require("moment");
 
 router.get("/:first_name/:last_name", (request, response, next) => {
   const { first_name, last_name } = request.params;
@@ -15,6 +16,17 @@ router.get("/:first_name/:last_name", (request, response, next) => {
   );
 });
 
+router.get("/list", (request, response, next) => {
+  pool.query(
+    `select * from invitations where rsvp_yes_adults > 0 or rsvp_yes_kids > 0 or rsvp_no_adults > 0 order by rsvp_time`,
+    [first_name, last_name, first_name, last_name],
+    (err, res) => {
+      if (err) return next(err);
+      response.json(res.rows);
+    }
+  );
+});
+
 router.put("/:id", (request, response, next) => {
   const { id } = request.params;
   const keys = Object.keys(request.body);
@@ -23,6 +35,8 @@ router.put("/:id", (request, response, next) => {
   keys.forEach(key => {
     if (request.body[key] !== null) fields.push(key);
   });
+  const timestamp = moment().format();
+
   fields.forEach((field, index) => {
     pool.query(
       `UPDATE invitations SET ${field}=($1) WHERE id=$2`,
@@ -30,7 +44,14 @@ router.put("/:id", (request, response, next) => {
       (err, res) => {
         if (err) return next(err);
         if (index === fields.length - 1) {
-          response.send("it was updated");
+          pool.query(
+            `UPDATE invitations SET rsvp_time = current_timestamp WHERE id=$1`,
+            [id],
+            (err, res) => {
+              if (err) return next(err);
+              response.send("it was updated");
+            }
+          );
         }
       }
     );
